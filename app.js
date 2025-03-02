6a23d2f4272d4a729574f847e58e264b3aca3569
Adnan-Kharadi <adnankharadi8@gmail.com>
2025-03-03 03:13:17 +0500

    Java Files

---
+++ app.js
@@ -0,0 +1,190 @@
+// // / Assignment No : 6
+
+// // MATH EXPRESSIONS
+// // Q1
+//  var a = 10;
+//  document.write("Result: <br>");
+
+// document.write("The value of a is: ", a , "<br>");
+
+// document.write("-------------------------- <br>");
+
+// document.write("The value of ++a is: ", ++a , "<br>");
+
+// document.write("Now the value of a is: ", a , "<br> <br>");
+
+// document.write("The value of a++ is: ", a++ , "<br>");
+// document.write("Now the value of a is: ", a , "<br> <br>");
+
+
+// document.write("The value of --a is: ", --a , "<br>");
+// document.write("Now the value of a is: ", a , "<br> <br>");
+
+// document.write("The value of a-- is: ", a-- , "<br>");
+// document.write("Now the value of a is: ", a , "<br> <br>");
+
+
+// // Q2
+// var a =2, b=1;
+// var result = --a - --b + ++b + b--;
+
+// document.write("result is ", result);
+
+// // Q3
+// var userName = prompt("Enter your name");
+// alert("Welcome "+ userName);
+
+// // Q5
+// var number = +prompt ("Enter number for multiplication");
+// if (!number){
+//     number = 5;
+// }
+
+// document.write(number," x 1 = ", number*1, "<br>", number," x 2 = ", number*2, "<br>", number," x 3 = ", number*3, "<br>", number," x 4 = ", number*4, "<br>", number," x 5 = ", number*5, "<br>", number," x 6 = ", number*6, "<br>", number," x 7 = ", number*7, "<br>", number," x 8 = ", number*8, "<br>", number," x 9 = ", number*9, "<br>", number," x 10 = ", number*10
+// );
+
+
+
+// // Q6
+// // a)
+// var sub1 = prompt ("Enter subject 1");
+// var sub2 = prompt ("Enter subject 2");
+// var sub3 = prompt ("Enter subject 3");
+
+// // b)
+// var totalMarks = 100;
+
+// // c)
+// var obtainedMarks1 = +prompt ("Enter your "+ sub1+ " marks");
+
+// // d)
+// var obtainedMarks2 = +prompt ("Enter your "+ sub2+ " marks");
+// var obtainedMarks3 = +prompt ("Enter your "+ sub3+ " marks");
+
+// // e)
+// document.write(`<table>
+//     <tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th><th>Percentage</th>
+//     </tr><tr><td>${sub1}</td><td>${totalMarks}</td><td>${obtainedMarks1}</td><td>${(obtainedMarks1/totalMarks)*100}</td></tr>
+//     <tr><td>${sub2}</td><td>${totalMarks}</td><td>${obtainedMarks2}</td><td>${(obtainedMarks2/totalMarks)*100}</td></tr>
+//     <tr><td>${sub3}</td><td>${totalMarks}</td><td>${obtainedMarks3}</td><td>${(obtainedMarks3/totalMarks)*100}</td></tr>
+
+//     <tr><td>}</td><td>${totalMarks*3}</td><td>${obtainedMarks1+obtainedMarks2+obtainedMarks3}</td><td>${((obtainedMarks1+obtainedMarks2+obtainedMarks3)/300)*100}</td></tr>
+//     </table>`);
+
+
+
+//     // Home Assignment
+// // Chapter ; 6 (Math Expression II)
+
+// // Q1
+// x++;
+// x += 1;
+
+// //  Q2
+// x = 100;
+// --x;
+
+// // Q3
+// 50
+
+// // Q4
+// 49
+
+// // Q5
+//  var num = 50;
+// var newNum = num--;
+
+// // Q6
+//  var num = 50;
+//  var newNum = num++;
+
+// // Q7
+
+// // var value = 7;
+// // var inc = ++value;
+// // alert(inc);
+
+
+// // Chapter 7 (Math Expression III)
+
+// // Q1
+// // var calculatedNum = 2 + (2 * 6);
+// // 14
+
+// // Q2
+// // var calculatedNum = (2 + 2) * 6;
+// // 24
+
+// // Q3
+// // var calculatedNum = (2 + 2) * (4 + 2);
+// // 24
+
+// // Q4
+// // var calculatedNum = ((2 + 2) * 4) + 2;
+// // 18
+
+// // Q5
+// // var calculatedNum = (2 + 2) * (4 + 10);
+
+
+// // Q6
+// // var calculatedNum = 2 + (2 * 4) + 10;
+
+
+// // // Q7
+// // var calculatedNum = 4 /( 2 * 4);
+// // this will produce 0.5
+
+
+// // Chapter 8 (Concatenating Text Strings)
+
+// // Q1
+// var num = "2"+"2";
+// alert(num)
+// 22
+
+// // Q2
+// var message = "Hello," + "Dolly";
+// alert(message);
+//  Hello,Dolly
+
+// // Q3
+// alert("33" + 3);
+// 333
+
+// // Q4
+
+// alert("Pakistan " + "Zindabad");
+
+// // Q5
+// var concatination = 123456 + "@gmail.com";
+
+// // Q6
+// var alpha = "Hello";
+// var bravo = "World";
+// var charlie = alpha + bravo;
+// alert(charlie);
+
+
+// // Chapter 9 (Prompts)
+
+// // Q1
+// var firstName = prompt ("Enter first name");
+
+// // Q2
+// var country = prompt ("Enter country", "China");
+
+// // Q3
+// var yourName = prompt("Enter Your Name");
+
+// // Q4
+// var message = prompt("hello welcome..");
+
+// // Q5 and Q6
+// var firstMessage = "Hello gentleman";
+// var secondMessage = "Type anything";
+// var show = prompt (firstMessage);
+// // if (!show){
+//     show = secondMessage;
+// // }
+// alert(show);
